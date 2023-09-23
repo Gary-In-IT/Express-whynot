5057e470defd9cff97366ca9e16038ce8bf3fdda
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-09-23 11:42:47 -0400

    playing with pug,
    meh, not impressed so far

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const express = require("express");
 
 const app = express();
 
+// const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+
 app.set("view engine", "pug");
 
-// render from the pug view file
+// render from the view file
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<h1>Hello Bro!!</h1>");
+app.get("/cards", (req, res) => {
+  res.render("card", {
+    prompt: "Who is buried in Grant's tomb?",
+  });
 });
 
 app.listen(3000, () => {
