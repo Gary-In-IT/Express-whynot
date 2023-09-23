726b48b8b0f1000ed572cb60d58d3985349e4052
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-09-23 16:34:48 -0400

    meh, pug is not my thing
    gonna stick to react

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
 
@@ -9,6 +11,14 @@ app.set("view engine", "pug");
 // render from the view file
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/hello", (req, res) => {
+  res.render("hello");
+});
+
+app.post("/hello", (req, res) => {
+  res.render("hello", { name: req.body.username });
 });
 
 app.get("/cards", (req, res) => {
