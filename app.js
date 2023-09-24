d1bb65184c641061841d51a8b69f73d9e7fbd5f0
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-09-24 18:37:38 -0400

    res,req,next  and so on
    redee

--- app.js
+++ app.js
@@ -1,30 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
 
 app.set("view engine", "pug");
 
+// middleware
+app.use((req, res, next) => {
+  console.log("hello");
+  next();
+});
+
 // render from the view file
 app.get("/", (req, res) => {
-  res.render("index");
+  const name = req.cookies.username;
+  if (name) {
+    res.render("index", { name });
+  } else {
+    res.redirect("/hello");
+  }
 });
 
 app.get("/hello", (req, res) => {
-  res.render("hello");
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect("/");
+  } else {
+    res.render("hello");
+  }
 });
 
 app.post("/hello", (req, res) => {
-  res.render("hello", { name: req.body.username });
+  res.cookie("username", req.body.username);
+  res.redirect("/");
 });
 
 app.get("/cards", (req, res) => {
   res.render("card", {
     prompt: "Who is buried in Grant's tomb?",
   });
+});
+
+app.post("/goodbye", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/hello");
 });
 
 app.listen(3000, () => {
